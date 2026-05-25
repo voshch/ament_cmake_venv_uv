809edefa34d03dcafdf0b88e81e0f369d097d88a
Vova Shcherbyna <dev@voshch.dev>
2026-05-25 20:57:47 +0200

    stamp venv with .built marker so failed builds retry

--- cmake/uv_venv.cmake
+++ cmake/uv_venv.cmake
@@ -22,25 +22,27 @@ function(uv_venv)
             "${venv_build_dir}"
     )
 
+    set(_stamp "${venv_dir}/.built")
     set(_cmd
         "export PYTHONPATH=''"
         "cd ${venv_build_dir}"
         "uv venv --clear ${venv_dir}"
         ". ${venv_dir}/bin/activate"
         "uv pip install ${venv_build_dir}"
+        "touch ${_stamp}"
     )
 
     list(JOIN _cmd " && " _cmd)
 
     add_custom_command(
-        OUTPUT "${venv_dir}"
+        OUTPUT "${_stamp}"
         DEPENDS "${venv_build_dir}"
         COMMAND sh -c "${_cmd}"
         VERBATIM
     )
 
     add_custom_target("${target_venv}" ALL
-        DEPENDS "${venv_dir}"
+        DEPENDS "${_stamp}"
     )
 
     venv_install()
